5ba558f08e83a447c73d89f81e6dece62984779f
Jay Johnson <raphael78@gmail.com>
2022-01-15 18:24:30 -0700

    CMS project added

--- WDD430-app/src/app/app.module.ts
+++ WDD430-app/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { WarningComponent } from './warning/warning.component';
+import { SuccessComponent } from './success/success.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarningComponent,
+    ServerComponent,
+    SuccessComponent,
   ],
   imports: [
     BrowserModule,
