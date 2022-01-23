035780870e9618ff4ceb723ab7b772c6b3bf2913
Jay Johnson <raphael78@gmail.com>
2022-01-22 23:35:51 -0700

    Message component
    Added functionality to Messages feature

--- cms/src/app/messages/message-edit/message-edit.component.ts
+++ cms/src/app/messages/message-edit/message-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'cms-message-edit',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./message-edit.component.css']
 })
 export class MessageEditComponent implements OnInit {
+  @ViewChild('subject', { static:false }) subjectRef: ElementRef;
+  @ViewChild('msgText', { static:false }) msgTextRef: ElementRef;
+  @Output() addMessageEvent = new EventEmitter<Message>();
+  currentSender:string = 'Jay';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSendMessage() {
+    const subjectValue = this.subjectRef.nativeElement.value;
+    const msgTextValue = this.msgTextRef.nativeElement.value;
+    const messageObj = new Message('1', subjectValue, msgTextValue, this.currentSender );
+    this.addMessageEvent.emit(messageObj);
+  }
+
+  onClear() {
+
   }
 
 }
--- cms/src/app/messages/message-list/message-list.component.ts
+++ cms/src/app/messages/message-list/message-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'cms-message-list',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./message-list.component.css']
 })
 export class MessageListComponent implements OnInit {
+  messages: Message[] = [
+    new Message('11', 'Grades', 'The grades for this assignment have been posted', 'Bro. Jackson'),
+    new Message('12', 'Grades', 'When is assignment 3 due', 'Steve Johnson'),
+    new Message('13', 'Grades', 'Assignment 3 is due on Saturday at 11:30 PM', 'Bro. Jackson'),
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onAddMessage(message: Message)  {
+    this.messages.push(message);
+    console.log('message: ', message);
   }
 
 }
