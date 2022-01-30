ba3bcc918d1a5cbcb74f1aa14a3a6fba9791d66d
Jay Johnson <raphael78@gmail.com>
2022-01-29 21:03:02 -0700

    Updated documents

--- cms/src/app/documents/document-list/document-list.component.ts
+++ cms/src/app/documents/document-list/document-list.component.ts
@@ -10,10 +10,10 @@ export class DocumentListComponent implements OnInit {
   @Output() selectedDocumentEvent = new EventEmitter<Document>();
 
   documents: Document[] = [
-    new Document(1, 'Homework 1', 'Assignment 1 for class', 'http://www.homework.com/assignment/1', []),
-    new Document(2, 'Homework 2', 'Assignment 2 for class', 'http://www.homework.com/assignment/2', []),
-    new Document(3, 'Homework 3', 'Assignment 3 for class', 'http://www.homework.com/assignment/3', []),
-    new Document(4, 'Homework 4', 'Assignment 4 for class', 'http://www.homework.com/assignment/4', []),
+    new Document(1, 'Amazing Spider-Man #361', 'First appearance of Carnage', 'https://www.marvel.com/comics/issue/6773/the_amazing_spider-man_1963_361', []),
+    new Document(2, 'X-Men #1', 'New X-Men series, cover by Jim Lee', 'https://www.marvel.com/comics/issue/14281/x-men_1991_1', []),
+    new Document(3, 'Infinity Gauntlet #1', 'First in a 6 issue series', 'https://www.marvel.com/comics/issue/9286/infinity_gauntlet_1991_1', []),
+    new Document(4, 'Fantastic Four #49', 'Arrival of Galactus and the Silver Surfer', 'https://www.marvel.com/comics/issue/13254/fantastic_four_1961_49', []),
   ];
 
   constructor() { }
