3090de24d991df3a09b24abc7ae521c96d7ccd42
Jay Johnson <raphael78@gmail.com>
2022-02-19 00:06:55 -0700

    Week 7

--- cms/src/app/contacts/contact.service.ts
+++ cms/src/app/contacts/contact.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter, Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 import { Contact } from './contact.model';
 import { MOCKCONTACTS } from './MOCKCONTACTS';
 
@@ -9,9 +10,13 @@ export class ContactService {
   contactSelectedEvent = new EventEmitter<Contact>();
   contactChangedEvent = new EventEmitter<Contact[]>();
   contacts: Contact[] = [];
+  maxContactId: number;
+
+  contactListChangedEvent = new Subject<Contact[]>();
 
   constructor() { 
     this.contacts = MOCKCONTACTS;
+    this.maxContactId = this.getMaxId();
   }
 
   getContacts() {
@@ -32,9 +37,48 @@ export class ContactService {
     if(!contact) return;
 
     const pos = this.contacts.indexOf( contact );
+
+    if(pos < 0 ) return;
+    this.contacts.splice(pos, 1);
+
+    let contactListClone = this.contacts.slice();
+    this.contactListChangedEvent.next(contactListClone);
+  }
+
+  addContact(newContact: Contact)  {
+    if(!newContact) return;
+
+    newContact.id = String(this.maxContactId++);
+    this.contacts.push(newContact);
+
+    let contactListClone = this.contacts.slice();
+    this.contactListChangedEvent.next(contactListClone);
+  }
+
+  updateContact(originalContact: Contact, newContact: Contact) {
+    if(!originalContact || !newContact) return;
+
+    let pos = this.contacts.indexOf(originalContact);
     if(pos < 0 ) return;
 
-    this.contacts.splice(pos, 1);
-    this.contactChangedEvent.emit( this.contacts.slice() );
+    newContact.id = originalContact.id;
+    this.contacts[pos] = newContact;
+
+    let contactListClone = this.contacts.slice();
+    this.contactListChangedEvent.next(contactListClone);
+  }
+
+  getMaxId(): number{
+    let maxId = 0;
+
+    for(let i = 0; i > this.contacts.length; i++) {
+      let c = this.contacts[i];
+      let currentId = parseInt(c.id);
+
+      if(currentId > maxId) {
+        maxId = currentId;
+      }
+    }
+    return maxId;
   }
 }
--- cms/src/app/documents/document.service.ts
+++ cms/src/app/documents/document.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter, Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 import { Document } from './document.model';
 import { MOCKDOCUMENTS } from './MOCKDOCUMENTS';
 
@@ -9,9 +10,13 @@ export class DocumentService {
   documentSelectedEvent = new EventEmitter<Document>();
   documentChangedEvent = new EventEmitter<Document[]>();
   documents: Document[] =  [];
+  maxDocumentId: number;
+
+  documentListChangedEvent = new Subject<Document[]>();
 
   constructor() {
     this.documents = MOCKDOCUMENTS;
+    this.maxDocumentId = this.getMaxId();
   }
 
   getDocuments()  {
@@ -32,9 +37,48 @@ export class DocumentService {
     if(!document) return;
 
     const pos = this.documents.indexOf( document );
-    if(pos < 0) return;
 
+    if(pos < 0) return;
     this.documents.splice(pos, 1);
-    this.documentChangedEvent.emit( this.documents.slice() );
+
+    let documentsListClone = this.documents.slice();
+    this.documentListChangedEvent.next(documentsListClone);
+  }
+
+  addDocument(newDocument: Document) {
+    if(!newDocument) return;
+
+    newDocument.id = String(this.maxDocumentId++);
+    this.documents.push(newDocument);
+
+    let documentsListClone = this.documents.slice();
+    this.documentListChangedEvent.next(documentsListClone);
+  }
+
+  updateDocument(originalDocument: Document, newDocument: Document)  {
+    if(!originalDocument || !newDocument) return;
+
+    let pos = this.documents.indexOf(originalDocument);
+    if (pos < 0) return;
+
+    newDocument.id = originalDocument.id;
+    this.documents[pos] = newDocument;
+
+    let documentsListClone = this.documents.slice();
+    this.documentListChangedEvent.next(documentsListClone);
+  }
+
+  getMaxId(): number  {
+    let maxId = 0;
+
+    for(let i=0; i < this.documents.length; i++) {
+      let d = this.documents[i];
+      let currentId = parseInt(d.id);
+
+      if(currentId > maxId)  {
+        maxId = currentId;
+      }
+    }
+     return maxId;
   }
 }
