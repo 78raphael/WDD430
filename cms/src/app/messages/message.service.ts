f16b4fa9d5188a2b7c79f2b27bac63edfede7fc1
Jay Johnson <raphael78@gmail.com>
2022-03-09 19:50:12 -0700

    Week 9 Finished

--- cms/src/app/messages/message.service.ts
+++ cms/src/app/messages/message.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { EventEmitter, Injectable } from '@angular/core';
 import { Message } from './message.model';
 import { MOCKMESSAGES } from './MOCKMESSAGES';
@@ -6,23 +7,95 @@ import { MOCKMESSAGES } from './MOCKMESSAGES';
   providedIn: 'root'
 })
 export class MessageService {
-  messages: Message[] = [];
   messageChangedEvent = new EventEmitter<Message[]>();
+  messages: Message[] = [];
+  maxMessageId: number;
 
-  constructor() {
+  constructor(private http: HttpClient) {
     this.messages = MOCKMESSAGES;
+    this.maxMessageId = this.getMaxId();
   }
 
   getMessages() {
-    return this.messages.slice();
+    try {
+      return this.http
+        .get<Message[]>('https://wdd430-angular-cms-default-rtdb.firebaseio.com/messages.json',
+          {
+            headers: new HttpHeaders({ 'Custom-Header': 'Messages' })
+          }
+        )
+        .subscribe({
+          next:
+          (messages: Message[]) => {
+            this.messages = messages;
+            this.maxMessageId = this.getMaxId();
+            this.messages.sort((a, b) => {
+              if(a < b) return -1;
+              if(a > b) return 1;
+              return 0;
+            });
+
+            let messagesListClone = this.messages.slice();
+            this.messageChangedEvent.next(messagesListClone);
+          },
+          error:
+          (error: any) => {
+            console.log("Error: ", error);
+          }
+        });
+    } catch(error)  {
+      console.error('Error (getMessages): ', error);
+    }
   }
 
-  getMessage(id: string)  {
+  storeMessages()  {
+    try {
+      let stringMessages = JSON.stringify(this.messages);
 
+      this.http.put('https://wdd430-angular-cms-default-rtdb.firebaseio.com/messages.json',
+        stringMessages,
+        {
+          headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+        })
+        .subscribe({
+          next:
+          () => {
+            let messagesListClone = this.messages.slice();
+            this.messageChangedEvent.next(messagesListClone);
+          },
+          error:
+          (error: any) => {
+            console.log("Error: ", error);
+          }
+        });
+
+
+    } catch(error)  {
+      console.log('Error (storeMessages): ', error);
+    }
   }
 
-  addMessage(message: Message)  {
-    this.messages.push(message);
-    this.messageChangedEvent.emit(this.messages.slice());
+  addMessage(newMessage: Message)  {
+    if(!newMessage) return;
+    this.maxMessageId++;
+
+    newMessage.id = String(this.maxMessageId);
+    this.messages.push(newMessage);
+
+    this.storeMessages();
+  }
+
+  getMaxId(): number  {
+    let maxId = 0;
+
+    for(let i=0; i < this.messages.length; i++) {
+      let d = this.messages[i];
+      let currentId = parseInt(d.id);
+
+      if(currentId > maxId)  {
+        maxId = currentId;
+      }
+    }
+     return maxId;
   }
 }
